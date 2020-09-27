f049d829c7cdc34847a6e2b3f2778b5a6299664e
Administrator <admin@example.com>
2020-09-27 18:35:52 -0500

    Se agrego las funciones de deteccion

--- main.py
+++ main.py
@@ -5,35 +5,148 @@ from PIL import ImageTk, Image
 import numpy as np
 import pandas as pd
 import cv2
-import datetime
+import datetime, time
+import sqlite3
+from threading import Thread
 
 dummy_file=pd.DataFrame({"FECHA":['2020-01-01', '2020-01-02'], "ACTIVIDAD":["ACCESO NO AUTORIZADO DETECTADO", "ACCESO NO AUTORIZADO DETECTADO"]})
+
+class ThreadedVideo():
+    def __init__(self, src, detection_params):
+        # Detection 
+        self.detection_params=detection_params
+        self.capture = cv2.VideoCapture(src)
+        self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 2)
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = Darknet(self.detection_params["model_def"], img_size=self.detection_params["img_size"]).to(self.device)
+
+        
+        if self.detection_params["weights_path"].endswith(".weights"):
+            model.load_darknet_weights(self.detection_params["weights_path"])
+        else:
+            model.load_state_dict(torch.load(self.detection_params["weights_path"]))
+
+        model.eval()  
+        classes = load_classes(self.detection_params["class_path"])
+        Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+        
+        self.colors = np.random.randint(0, 255, size=(len(self.classes), 3), dtype="uint8")
+        self.a=[]
+        # FPS = 1/X
+        # X = desired FPS
+        self.FPS = 1/30
+        self.FPS_MS = int(self.FPS * 1000)
+
+        # Start frame retrieval thread
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+        self.thread.start()
+
+    def update(self):
+        while True:
+            if self.capture.isOpened():
+                self.status, self.frame = self.capture.read()
+            time.sleep(self.FPS)
+
+    def show_frame(self):
+        cv2.imshow('frame', self.frame)
+        cv2.waitKey(self.FPS_MS)
+    
+    def return_frame(self):
+        self.frame=cv2.resize(self.frame,(848,480), fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
+        return self.frame
+
+    def detect(self,frame):
+
+        flag_no_autorizado=False
+
+        RGBimg=Convertir_RGB(frame)
+        imgTensor = transforms.ToTensor()(RGBimg)
+        imgTensor, _ = pad_to_square(imgTensor, 0)
+        imgTensor = resize(imgTensor, 416)
+        imgTensor = imgTensor.unsqueeze(0)
+        imgTensor = Variable(imgTensor.type(Tensor))
+
+
+        with torch.no_grad():
+            detections = self.model(imgTensor)
+            detections = non_max_suppression(detections, self.detection_params["conf_thres"], self.detection_params["nms_thres"])
+
+
+        for detection in detections:
+            if detection is not None:
+                detection = rescale_boxes(detection, self.detection_params["img_size"], RGBimg.shape[:2])
+                for x1, y1, x2, y2, conf, cls_conf, cls_pred in detection:
+                    box_w = x2 - x1
+                    box_h = y2 - y1
+                    color = [int(c) for c in colors[int(cls_pred)]]
+                    print("Se detectó {} en X1: {}, Y1: {}, X2: {}, Y2: {}".format(classes[int(cls_pred)], x1, y1, x2, y2))
+                    frame = cv2.rectangle(frame, (x1, y1 + box_h), (x2, y1), color, 3)
+                    cv2.putText(frame, classes[int(cls_pred)], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 3)# Nombre de la clase detectada
+                    cv2.putText(frame, str("%.2f" % float(conf)), (x2, y2 - box_h), cv2.FONT_HERSHEY_SIMPLEX, 0.5,color, 3) # Certeza de prediccion de la clase
+
+                    if classes[int(cls_pred)]='no_autorizado':
+                        flag_no_autorizado=True
+        
+        # esta pendiente reconvertira RBG
+        return frame, flag_no_autorizado
+
+    def Convertir_RGB(img):
+        # Convertir Blue, green, red a Red, green, blue
+        b = img[:, :, 0].copy()
+        g = img[:, :, 1].copy()
+        r = img[:, :, 2].copy()
+        img[:, :, 0] = r
+        img[:, :, 1] = g
+        img[:, :, 2] = b
+        return img
+
+
+    def Convertir_BGR(img):
+        # Convertir red, blue, green a Blue, green, red
+        r = img[:, :, 0].copy()
+        g = img[:, :, 1].copy()
+        b = img[:, :, 2].copy()
+        img[:, :, 0] = b
+        img[:, :, 1] = g
+        img[:, :, 2] = r
+        return img
 
 class App:
     def __init__(self, window):
 
         # Config params
-        
-        self.model_def="config/yolov3.cfg" #path to model definition file
-        self.weights_path="weights/yolov3.weights" #path to weights file
-        self.class_path="data/coco.names" #path to class label file
-        self.conf_thres=0.8 #object confidence threshold
-        self.nms_thres=0.4 #iou thresshold for non-maximum suppression
-        self.n_cpu=0 #number of cpu threads to use during batch generation
-        self.img_size=416 #size of each image dimension
-        self.directorio_video="" #Directorio al video
-        self.checkpoint_model="" #path to checkpoint model
+        self.detection_params={
+
+        "model_def"="config/yolov3.cfg" #path to model definition file
+        ,"weights_path"="weights/yolov3.weights" #path to weights file
+        ,"class_path"="data/coco.names" #path to class label file
+        ,"conf_thres"=0.8 #object confidence threshold
+        ,"nms_thres"=0.4 #iou thresshold for non-maximum suppression
+        ,"n_cpu"=0 #number of cpu threads to use during batch generation
+        ,"img_size"=416 #size of each image dimension
+        ,"directorio_video"="" #Directorio al video
+        ,"checkpoint_model"="" #path to checkpoint model
+        }
+
         self.ipcam_url = 'rtsp://admin:Cuarentena2020@192.168.100.201:554/Streaming/Channels/1'
+        self.video_url = 'videos/1.mp4'
         self.fecha_hora_ult_detectado=None
-        self.alert_from_email='juanpalomino@gmail.com'
-        self.alert__from_email_password='juanpalomino@gmail.com'
-        self.alert_to_email='juanpalomino@gmail.com'
-        
+        self.alert_from_email='alertas.rtsb@gmail.com'
+        self.alert__from_email_password='Cuarentena.2020'
+        self.alert_to_email='alertas.rtsb@gmail.com'
+        self.db='example.db'
+
+        self.video_thread=None
+
+        conn = sqlite3.connect('example.db')
 
         #Window
         self.window = window
         self.window.title('DETECTOR DE USUARIOS PERMITIDOS')
-        self.window.geometry('1366x768')
+        self.window.geometry('855x590')
 
         # styling
         ttk.Style().configure("TButton", padding=6, relief="flat",   background="#ccc")
@@ -51,7 +164,7 @@ class App:
 
         # ROW 2
         # Video Placeholder (comentar cuando se utilice camara ip)
-        path = "./images/cctv.jpg"
+        path = "./images/placeholder.png"
         self.imageFrame = Frame(window, width=1280, height=720)
         self.imageFrame.grid(column=0, row=2, columnspan=6)
         # self.imageFrame.grid(row=0, column=0, padx=10, pady=2)
@@ -89,9 +202,34 @@ class App:
             with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
                 try:
                     server.login(self.alert_from_email, self.alert_from_email_password)
-                    server.sendmail(self.alert_from_email, self.alert_to_email, "Se detecto acceso no autorizado, Fecha y Hora: {}".format(hora_actual))
+                    server.sendmail(self.alert_from_email, self.alert_to_email, "Se detecto acceso no autorizado, Fecha y Hora: {}".format(hora_actual.strftime("%d/%m/%Y %H:%M:%S")))
+                    server.close()
                 except Exception as e:
-                    
+                    print("Error al enviar el mail")
+            
+            conn = None
+            try:
+                conn = sqlite3.connect(db_file)
+                c=conn.cursor
+                c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='bitacora_accesos' ''')
+
+                if c.fetchone()[0]==1:
+                    c.execute('''INSERT INTO bitacora_accesos VALUES ('{}', '{}') '''.format(hora_actual.strftime("%d/%m/%Y %H:%M:%S"), "ACCESO NO AUTORIZADO"))
+                # print(sqlite3.version)
+                else:
+                    c.execute(''' CREATE TABLE bitacora_accesos  (FECHA_HORA TEXT,EVENTO TEXT) ''')
+                    c.execute('''INSERT INTO bitacora_accesos VALUES ('{}', '{}') '''.format(hora_actual.strftime("%d/%m/%Y %H:%M:%S"), "ACCESO NO AUTORIZADO"))
+                
+                conn.commit()
+            
+            except Error as e:
+                print(e)
+            
+            finally:
+                if conn:
+                    conn.close()
+
+            
 
     def save(self): 
         files = [('All Files', '*.*'),  
@@ -129,10 +267,13 @@ class App:
         self.edit_window.title('ABOUT')
 
     def detect_from_ipcam(self):
-        _, frame = self.cap.read()
+        if self.video_thread==None:
+            self.video_thread=ThreadedVideo(ipcam_url)
+        # _, frame = self.cap.read()
         # if _ ==True:
         try:
-            frame=cv2.resize(frame,(1300,720), fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
+            # frame=cv2.resize(frame,(1300,720), fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
+            frame=self.video_thread.return_frame()
             # else:
             #     break
             # frame = cv2.flip(frame, 1)
@@ -146,8 +287,26 @@ class App:
             print(e)
         self.panel.after(10, self.detect_from_ipcam)
 
-    def detect_from_video():
-        pass
+    def detect_from_video(self):
+        if self.video_thread==None:
+            self.video_thread=ThreadedVideo(self.video_url)
+        # _, frame = self.cap.read()
+        # if _ ==True:
+        try:
+            # frame=cv2.resize(frame,(1300,720), fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
+            frame=self.video_thread.return_frame()
+            # else:
+            #     break
+            # frame = cv2.flip(frame, 1)
+            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+            img = Image.fromarray(cv2image)
+            imgtk = ImageTk.PhotoImage(image=img)
+            self.panel.imgtk = imgtk
+            self.panel.configure(image=imgtk)
+            # self.panel.pack()
+        except Exception as e:
+            print(e)
+        self.panel.after(10, self.detect_from_ipcam)
 
 
 if __name__ == "__main__":
